0729bf4ba19b824afc64a3c624a55f18ca7c58d7
lucastlw <lucaslemosw@gmail.com>
2017-12-18 15:33:57 -0200

    Class 1

--- class1/hello.c
+++ class1/hello.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 int main(void) {
+  int ret = 0;
+
 	printf("google.com.br\n");
-	printf("see you in the class 1\n");
+  printf("%x - %x - %x - %x\n", ret, 10, 0xa, 'A');
 	return 0;
 }
